b298a40a2582d622f13338013274c7774b678f1f
Sugimoto Masayoshi <sugimoto.massayoshi@gmail.com>
2018-12-19 22:32:15 +0900

    Refactoring

--- src/Game/Actor.js
+++ src/Game/Actor.js
@@ -1,5 +1,4 @@
 function Actor(_id) {
-	let _mobilityComponent = EmptyComponent()
 	let _renderComponents = []
 	let _position = Vector2D(0.0, 0.0)
 
@@ -8,12 +7,10 @@ function Actor(_id) {
 	function setPosition(position) { _position = position }
 	function x() { return _position.x() }
 	function y() { return _position.y() }
-	function setMobilityComponent(component) { _mobilityComponent = component }
 	function addRenderComponent(component) { _renderComponents.push(component) }
 	function renderComponents() { return _renderComponents }
 
 	function updateBattle(deltaTimeMillisecond) {
-		_mobilityComponent.update(deltaTimeMillisecond)
 	}
 
 	return {
@@ -22,13 +19,8 @@ function Actor(_id) {
 		setPosition,
 		x,
 		y,
-		setMobilityComponent,
 		addRenderComponent,
 		updateBattle,
 		renderComponents
 	}
-}
-
-function EmptyComponent() {
-	return {update: (deltaTimeMillisecond) => {}}
 }
--- src/Game/MobilitySystem.js
+++
@@ -1,8 +0,0 @@
-function MobilitySystem(_componentSystem) {
-
-	const _components = []
-	const _event = []
-
-	function addComponent(actor, position) {
-	}
-}
